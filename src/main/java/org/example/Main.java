3052258843925128021bc35a792d1402008856e7
HomeWork <amlerok96@gmail.com>
2025-02-19 00:12:19 +0400

    Fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,41 +2,42 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-            int a = 10;
-            int b = 20;
-            double c = 30;
-            int g = -2147483648;
-            int l = 2147483647;
+        int a = 10;
+        int b = 20;
+        double c = 30;
+        int x = Integer.MAX_VALUE;
+        int y = x + 1;
 
+        //0) применить несколько арифметических операций ( + , -, * , /) над двумя примитивами типа int
+        //1) применить несколько арифметических операций над int и double в одном выражении
+        int summa = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        double summ1 = a + c;
+        double diff1 = c - b;
+        System.out.println(summa);
+        System.out.println(diff);
+        System.out.println(mul);
+        System.out.println(summ1);
+        System.out.println(diff1);
 
+        //2) применить несколько логических операций ( < , >, >=, <= )
+        if (a > b) {
+            System.out.println("Победа!");
+        }
 
-            int summ = a + b;
-            int diff = a - b;
-            int mul = a * b;
-            double summ1 = a + c;
-            double diff1 = c - b;
-            System.out.println(summ);
-            System.out.println(diff);
-            System.out.println(mul);
-            System.out.println(summ1);
-            System.out.println(diff1);
+        if (a < b) {
+            System.out.println("Еще одна победа!");
+        }
 
-            if (a > b) {
-                System.out.println("Победа!");
-            }
+        if (a == b) {
+            System.out.println("Ничья!");
+        } else {
+            System.out.println("Судья!");
+        }
 
-            if (a < b) {
-                System.out.println("Еще одна победа!");
-            }
-
-            if (a == b) {
-                System.out.println("Ничья!");
-            }else {
-                System.out.println("Судья!");
-            }
-
-            System.out.println(g*1);
-
-            System.out.println(l*1);
+        //Переполнение
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
     }
 }
